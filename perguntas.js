b4fedc744ab24b0d49805a92178cdffb858c5c2e
r4isales <raissa.sales@escola.pr.gov.br>
2025-05-26 08:55:56 +0000

    6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'A Coca-Cola já teve cocaína na receita original?',
+    'Ela não tinha cocaína pura, mas usava folhas de coca, contendo cerca de 9 mg de cocaína por copo, sendo totalmente removida em 1905'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Em quais países a Coca-Cola não é vendida oficialmente?',
+    'Atualmente, a Coca-Cola não é vendida oficialmente na Coreia do Norte e em Cuba'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Por quanto tempo uma Coca-Cola custou 5 centavos?',
+    'De 1886 até 1959, o preço de uma Coca-Cola foi de 5 centavos nos Estados Unidos'
+)
