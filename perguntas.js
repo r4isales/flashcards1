125451df6ea4e723018c074335c2659f7fadff34
r4isales <raissa.sales@escola.pr.gov.br>
2025-05-27 11:17:17 +0000

    final

--- perguntas.js
+++ perguntas.js
@@ -20,3 +20,38 @@ criaCartao(
     'Como a Coca-Cola se chama na China e o que significa?',
     'Na China, a Coca-Cola se chama Kekoukele, que significa diversão saborosa'
 )
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Quando a Coca-Cola foi criada?',
+    'A Coca-Cola foi criada em 1886 por John Pemberton em Atlanta, nos Estados Unidos'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Qual o refrigerante mais vendido do mundo?',
+    'A Coca-Cola é o refrigerante mais vendido do mundo, presente em mais de 200 países'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Quantos produtos a Coca-Cola Company possui atualmente?',
+    'A empresa possui mais de 500 marcas e cerca de 3.500 bebidas diferentes ao redor do mundo'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Por que a Coca-Cola se chama assim?',
+    'O nome vem de dois ingredientes originais da receita: as folhas de coca e as nozes de cola'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'Qual a marca registrada mais famosa e reconhecida no mundo?',
+    'A marca Coca-Cola é considerada a mais reconhecida e valiosa globalmente desde o século XX'
+)
+
+criaCartao(
+    'Curiosidades Coca-Cola',
+    'A Coca-Cola já foi usada como remédio?',
+    'Sim, no início, a Coca-Cola era vendida em farmácias como um tônico para dores de cabeça e cansaço'
+)
